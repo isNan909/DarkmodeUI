c03dd34212ef71689d17f88803668f3c22901658
Ishan Manandhar <ishan02016@gmail.com>
2020-07-04 17:05:04 +0545

    added deak model toggle

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById('toogleInterface');
+const preferenceTheme = window.matchMedia("(preference-theme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btn.addEventListener("click", function () {
+  if (preferenceTheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
